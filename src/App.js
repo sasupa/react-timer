67bc3abf4b22b12ae6524a6d9a0380637d1a343f
Sasu Paakkunainen <sasupa@gmail.com>
2020-03-11 19:26:06 +0200

    Lisäsin header componentin

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from "react";
 import logo from "./logo.svg";
+import Header from "./Header/Header";
 import "./App.css";
 
 function App() {
-  return <div> SASUN JA JANIN HASSUTTELU :)) </div>;
-}
+  return (
+  <div>
+    <Header />
+    SASUN JA JANIN HASSUTTELU :)) 
+  </div>
+)}
 
 export default App;
---
+++ src/Header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Header.css';
+
+const header = () => {
+    return (
+        <div className="Header">
+        </div>
+    );
+};
+
+export default header;
