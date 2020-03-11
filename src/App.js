b6656481c749650c73ec6f03e5b3abfdffc3fef5
Jani Hellström <45870844+janihell@users.noreply.github.com>
2020-03-11 19:51:52 +0200

    Timer perusfunktionaalisuus ja css-roju

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import React from "react";
 import logo from "./logo.svg";
 import Header from "./Header/Header";
 import "./App.css";
+import Timer from "./containers/Timer";
 
 function App() {
   return (
-  <div>
-    <Header />
-    SASUN JA JANIN HASSUTTELU :)) 
-  </div>
-)}
+    <div>
+      <Header />
+      SASUN JA JANIN HASSUTTELU :))
+      <Timer />
+    </div>
+  );
+}
 
 export default App;
