e29db33b466036b23045431d2054b463b5ac0ef6
Sasu Paakkunainen <sasupa@gmail.com>
2020-03-11 19:29:44 +0200

    Vaihdoin kansiorakennetta

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import logo from "./logo.svg";
-import Header from "./Header/Header";
+import Header from "./components/Header/Header";
 import "./App.css";
 
 function App() {
